e842a17329b97471137d7baf6db079f0f1cd3da9
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-08-18 19:13:28 +0900

    処理結果の表示と現在の年月を自動的に表示するようにした

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,15 +10,20 @@ class BooksController < ApplicationController
   end
 
   def new
+    nowDate = Time.new
     @book = Book.new
+    @book.year = nowDate.year
+    @book.month = nowDate.mon
   end
 
   def create
     book_params = params.require(:book).permit(:year, :month, :inout, :category, :amount)
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "家計簿にデータを一件登録しました。"
       redirect_to books_path
     else
+      flash.now[:alert] = "登録に失敗しました。"
       render :new
     end
   end
@@ -31,8 +36,10 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     book_params = params.require(:book).permit(:year, :month, :inout, :category, :amount)
     if @book.update(book_params)
+      flash[:notice] = "データを一件更新しました"
       redirect_to books_path
     else
+      flash.now[:alert] = "更新に失敗しました。"
       render :edit
     end
   end
@@ -40,6 +47,7 @@ class BooksController < ApplicationController
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
+    flash[:notice] = "削除しました"
     redirect_to books_path
   end
 end
